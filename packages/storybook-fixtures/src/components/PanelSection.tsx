51b68903f72267131c1971525b4b42c7e94fd665
Tyom Semonov <inspiro@gmail.com>
2020-07-11 00:25:15 +0100

    Tweak variant button styles

--- packages/storybook-fixtures/src/components/PanelSection.tsx
+++ packages/storybook-fixtures/src/components/PanelSection.tsx
@@ -154,7 +154,7 @@ const FixtureButton = styled.button`
   border-radius: 0.3rem;
   padding: 0.2rem;
   transition: 100ms border-color;
-  font-size: 0.8rem;
+  font-size: 0.65rem;
   display: block;
   position: relative;
   text-align: left;
@@ -192,24 +192,24 @@ const FixtureButton = styled.button`
   .preview {
     border-radius: 0.2rem;
     padding: 0.2rem 1.4rem 0.2rem 0.2rem;
-    font-size: 0.6rem;
     background: #0001;
     overflow: hidden;
     white-space: pre-wrap;
+    line-height: 1.2;
     mask-image: linear-gradient(to bottom, #000a, transparent);
     position: absolute;
     top: 0;
     right: 0;
-    bottom: 1rem;
+    bottom: 0.5rem;
     left: 0;
   }
 
   .name {
+    font-size: 1.6em;
     position: absolute;
     font-weight: 600;
     bottom: 0.2rem;
     left: 0.2rem;
     padding: 0.2rem;
-    text-shadow: 0 1px 1px #0004;
   }
 `;
