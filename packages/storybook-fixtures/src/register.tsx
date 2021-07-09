239b4b93bb63124e7cb3ca2704826750c6b71f37
Tyom Semonov <inspiro@gmail.com>
2021-07-09 12:28:10 +0100

    Show fixture count in the add-on panel tab title

--- packages/storybook-fixtures/src/register.tsx
+++ packages/storybook-fixtures/src/register.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import addons, { types } from '@storybook/addons';
 import { AddonPanel } from '@storybook/components';
+import { useParameter } from '@storybook/api';
 import Panel from './components/Panel';
 import { ADDON_ID, PANEL_ID, PARAM_KEY } from '.';
 
 addons.register(ADDON_ID, () => {
   addons.addPanel(PANEL_ID, {
-    title: 'Fixtures',
+    title() {
+      const fixtures: {} = useParameter(PARAM_KEY) ?? {};
+      const fixtureCount = Object.keys(fixtures).length;
+      return `Fixtures ${fixtureCount ? `(${fixtureCount})` : ''}`;
+    },
     type: types.PANEL,
     paramKey: PARAM_KEY,
     render: ({ active = false, key }) => {
